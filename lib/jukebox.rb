eecf2147543de338d00f8526be61bd157f2dae14
David Zeit <dzeit216@gmail.com>
2018-05-06 20:27:59 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -28,7 +28,8 @@ def play(songs)
   puts "Please enter a song name or number:"
     user_input = gets.chomp
     if user_input 
-    
+  else 
+    puts "Invalid input, please try again"
 end 
 
 def exit 
